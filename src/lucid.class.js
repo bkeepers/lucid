8742a8d1c28a44c438164ce3652a061515abb454
Brandon Keepers <brandon@opensoul.org>
2011-02-14 23:55:21 -0500

    Simple implementation of classes

---
+++ src/lucid.class.js
@@ -0,0 +1,26 @@
+var Lucid;
+Lucid = Lucid || {};
+
+Lucid.Class = function() {
+};
+
+Lucid.Class.Methods = {
+  extend: function(properties) {
+    var Class = function() {
+      if(this.initialize) {
+        this.initialize.apply(this, arguments);
+      }
+    };
+
+    Class.prototype = _.extend(new this, properties);
+    _.extend(Class, this.Methods);
+    Class.prototype.constructor = Class;
+    return Class;
+  },
+
+  include: function() {
+    return _.extend.apply(_, [this.prototype].concat(_.toArray(arguments)));
+  }
+};
+
+_.extend(Lucid.Class, Lucid.Class.Methods);
